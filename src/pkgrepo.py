oracle/solaris-ips

34704015 Early KeyboardInterrupt generates NameError for EXIT_OOPS 35939853 pkg(7) utilities should use pkgdefs EXIT codes rather than hardcoded constants

src/pkgrepo.py
@@ -23,16 +23,56 @@
 #
 # Copyright (c) 2010, 2023, Oracle and/or its affiliates.
 #
-import pkg.no_site_packages
+
+try:
+    import pkg.no_site_packages
+
+    import atexit
+    import collections
+    import copy
+    import errno
+    import getopt
+    import gettext
+    import locale
+    import logging
+    import os
+    import operator
+    import shlex
+    import shutil
+    import six
+    import sys
+    import tempfile
+    import textwrap
+    import traceback
+    import warnings
+    import itertools
+    from importlib import reload
+
+    from pkg.client import global_settings
+    from pkg.client.debugvalues import DebugValues
+    from pkg.misc import msg, PipeError
+    from prettytable import PrettyTable
+    import pkg
+    import pkg.catalog
+    import pkg.client.api_errors as apx
+    import pkg.client.pkgdefs as pkgdefs
+    import pkg.client.progress
+    import pkg.client.publisher as publisher
+    import pkg.client.transport.transport as transport
+    import pkg.fmri as fmri
+    import pkg.misc as misc
+    import pkg.server.repository as sr
+    import rapidjson as json
+    from pkg.client.pkgdefs import (EXIT_OK, EXIT_OOPS, EXIT_BADOPT,
+        EXIT_PARTIAL, EXIT_FATAL)
+except KeyboardInterrupt:
+    import sys
+    sys.exit(1)  # EXIT_OOPS
 
 PKG_CLIENT_NAME = "pkgrepo"
 
-# pkgrepo exit codes
-EXIT_OK      = 0
-EXIT_OOPS    = 1
-EXIT_BADOPT  = 2
-EXIT_PARTIAL = 3
-EXIT_DIFF = 10
+# pkgrepo specific exit codes
+EXIT_DIFF = 10  # Differences found for pkgrepo diff.
 
 # listing constants
 LISTING_FORMATS = ("default", "json", "json-formatted", "tsv")
@@ -45,43 +85,6 @@ diff_type_f = {MINUS: "- ", PLUS: "+ ", COMMON: ""}
 
 # globals
 tmpdirs = []
-
-import atexit
-import collections
-import copy
-import errno
-import getopt
-import gettext
-import locale
-import logging
-import os
-import operator
-import shlex
-import shutil
-import six
-import sys
-import tempfile
-import textwrap
-import traceback
-import warnings
-import itertools
-from importlib import reload
-
-from pkg.client import global_settings
-from pkg.client.debugvalues import DebugValues
-from pkg.misc import msg, PipeError
-from prettytable import PrettyTable
-import pkg
-import pkg.catalog
-import pkg.client.api_errors as apx
-import pkg.client.pkgdefs as pkgdefs
-import pkg.client.progress
-import pkg.client.publisher as publisher
-import pkg.client.transport.transport as transport
-import pkg.fmri as fmri
-import pkg.misc as misc
-import pkg.server.repository as sr
-import rapidjson as json
 
 logger = global_settings.logger
 
@@ -128,7 +131,7 @@ def get_tracker(quiet=False):
     return progtrack
 
 
-def usage(usage_error=None, cmd=None, retcode=2, full=False):
+def usage(usage_error=None, cmd=None, retcode=EXIT_BADOPT, full=False):
     """Emit a usage message and optionally prefix it with a more
     specific error message.  Causes program to exit.
     """
@@ -2332,8 +2335,8 @@ def handle_errors(func, *args, **kwargs):
                 raise apx._convert_error(__e)
             error("\n" + misc.out_of_memory())
             __ret = EXIT_OOPS
-    except SystemExit as __e:
-        raise __e
+    except SystemExit:
+        raise
     except (IOError, PipeError, KeyboardInterrupt) as __e:
         # Don't display any messages here to prevent possible further
         # broken pipe (EPIPE) errors.
@@ -2357,10 +2360,10 @@ def handle_errors(func, *args, **kwargs):
     except (apx.ApiException, sr.RepositoryError) as __e:
         error(str(__e))
         __ret = EXIT_OOPS
-    except:
+    except Exception:
         traceback.print_exc()
         error(traceback_str)
-        __ret = 99
+        __ret = EXIT_FATAL
     return __ret
 
 

src/publish.py
@@ -24,32 +24,37 @@
 # Copyright (c) 2007, 2023, Oracle and/or its affiliates.
 #
 
-import pkg.no_site_packages
-import fnmatch
-import getopt
-import gettext
-import locale
-import os
-import sys
-import traceback
-import warnings
-import errno
-from pkg.client.pkgdefs import EXIT_OOPS, EXIT_OK, EXIT_PARTIAL, EXIT_BADOPT
+try:
+    import pkg.no_site_packages
+    import fnmatch
+    import getopt
+    import gettext
+    import locale
+    import os
+    import sys
+    import traceback
+    import warnings
+    import errno
+    from pkg.client.pkgdefs import (EXIT_OK, EXIT_OOPS, EXIT_BADOPT,
+        EXIT_PARTIAL, EXIT_FATAL)
 
-from importlib import reload
+    from importlib import reload
 
-import pkg.actions
-import pkg.bundle
-import pkg.client.api_errors as apx
-import pkg.fmri
-import pkg.manifest
-import pkg.misc as misc
-import pkg.publish.transaction as trans
-import pkg.client.transport.transport as transport
-import pkg.client.publisher as publisher
-from pkg.misc import msg, emsg, PipeError
-from pkg.client import global_settings
-from pkg.client.debugvalues import DebugValues
+    import pkg.actions
+    import pkg.bundle
+    import pkg.client.api_errors as apx
+    import pkg.fmri
+    import pkg.manifest
+    import pkg.misc as misc
+    import pkg.publish.transaction as trans
+    import pkg.client.transport.transport as transport
+    import pkg.client.publisher as publisher
+    from pkg.misc import msg, emsg, PipeError
+    from pkg.client import global_settings
+    from pkg.client.debugvalues import DebugValues
+except KeyboardInterrupt:
+    import sys
+    sys.exit(1)  # EXIT_OOPS
 
 nopub_actions = [ "unknown" ]
 
@@ -611,7 +616,7 @@ def trans_import(repo_uri, args, visitors=[]):
     except KeyError:
         print(_("No transaction ID specified in $PKG_TRANS_ID"),
             file=sys.stderr)
-        sys.exit(1)
+        sys.exit(EXIT_OOPS)
 
     opts, pargs = getopt.getopt(args, "T:", ["target="])
 
@@ -868,10 +873,10 @@ if __name__ == "__main__":
     except MemoryError:
         error("\n" + misc.out_of_memory())
         __ret = EXIT_OOPS
-    except SystemExit as _e:
-        raise _e
-    except:
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message())
-        __ret = 99
+        __ret = EXIT_FATAL
     sys.exit(__ret)

src/util/publish/pkgdiff.py
@@ -24,24 +24,30 @@
 # Copyright (c) 2009, 2023, Oracle and/or its affiliates.
 #
 
-import pkg.no_site_packages
-import getopt
-import gettext
-import locale
-import sys
-import traceback
-import warnings
-from functools import cmp_to_key
+try:
+    import pkg.no_site_packages
+    import getopt
+    import gettext
+    import locale
+    import sys
+    import traceback
+    import warnings
+    from functools import cmp_to_key
 
-import pkg.actions
-import pkg.variant as variant
-import pkg.client.api_errors as apx
-import pkg.manifest as manifest
-import pkg.misc as misc
-from pkg.misc import PipeError, CMP_UNSIGNED, CMP_ALL
-from collections import defaultdict
-from itertools import product
-from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_PARTIAL
+    import pkg.actions
+    import pkg.variant as variant
+    import pkg.client.api_errors as apx
+    import pkg.manifest as manifest
+    import pkg.misc as misc
+    from pkg.misc import PipeError, CMP_UNSIGNED, CMP_ALL
+    from collections import defaultdict
+    from itertools import product
+    from pkg.client.pkgdefs import (EXIT_OK, EXIT_OOPS, EXIT_BADOPT,
+        EXIT_PARTIAL, EXIT_FATAL)
+except KeyboardInterrupt:
+    import sys
+    sys.exit(1)  # EXIT_OOPS
+
 
 def usage(errmsg="", exitcode=EXIT_BADOPT):
     """Emit a usage message and optionally prefix it with a more specific
@@ -374,11 +380,11 @@ if __name__ == "__main__":
         exit_code = main_func()
     except (PipeError, KeyboardInterrupt):
         exit_code = EXIT_OOPS
-    except SystemExit as __e:
-        exit_code = __e
-    except Exception as __e:
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message(), exitcode=None)
-        exit_code = 99
+        exit_code = EXIT_FATAL
 
     sys.exit(exit_code)

src/util/publish/pkgfmt.py
@@ -23,7 +23,37 @@
 # Copyright (c) 2009, 2023, Oracle and/or its affiliates.
 #
 
-import pkg.no_site_packages
+try:
+    import pkg.no_site_packages
+
+    import copy
+    import errno
+    import getopt
+    import gettext
+    import io
+    import locale
+    import operator
+    import os
+    import re
+    import six
+    import sys
+    import tempfile
+    import traceback
+    import warnings
+    from difflib import unified_diff
+    from functools import cmp_to_key
+
+    import pkg
+    import pkg.actions
+    import pkg.misc as misc
+    import pkg.portable
+    from pkg.misc import emsg, PipeError
+    from pkg.actions.generic import quote_attr_value
+    from pkg.actions.depend import known_types as dep_types
+    from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_FATAL
+except KeyboardInterrupt:
+    import sys
+    sys.exit(1)  # EXIT_OOPS
 
 # Prefixes should be ordered alphabetically with most specific first.
 DRIVER_ALIAS_PREFIXES = (
@@ -52,37 +82,6 @@ DRIVER_ALIAS_PREFIXES = (
 # 5) variant & facet tags appear at the end of actions
 # 6) multi-valued tags appear at the end aside from the above
 # 7) key attribute tags come first
-
-try:
-    import copy
-    import errno
-    import getopt
-    import gettext
-    import io
-    import locale
-    import operator
-    import os
-    import re
-    import six
-    import sys
-    import tempfile
-    import traceback
-    import warnings
-    from difflib import unified_diff
-    from functools import cmp_to_key
-
-    import pkg
-    import pkg.actions
-    import pkg.misc as misc
-    import pkg.portable
-    from pkg.misc import emsg, PipeError
-    from pkg.actions.generic import quote_attr_value
-    from pkg.actions.depend import known_types as dep_types
-    from pkg.client.pkgdefs import (EXIT_OK, EXIT_OOPS, EXIT_BADOPT,
-        EXIT_PARTIAL)
-except KeyboardInterrupt:
-    import sys
-    sys.exit(EXIT_OOPS)
 
 FMT_V1 = "v1"
 FMT_V2 = "v2"
@@ -732,12 +731,12 @@ if __name__ == "__main__":
     except (PipeError, KeyboardInterrupt):
         # We don't want to display any messages here to prevent
         # possible further broken pipe (EPIPE) errors.
-        __ret = 1
-    except SystemExit as _e:
-        raise _e
-    except:
+        __ret = EXIT_OOPS
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message(), exitcode=None)
-        __ret = 99
+        __ret = EXIT_FATAL
 
     sys.exit(__ret)

src/util/publish/pkgmerge.py
@@ -23,9 +23,9 @@
 # Copyright (c) 2011, 2023, Oracle and/or its affiliates.
 #
 
-import pkg.no_site_packages
-
 try:
+    import pkg.no_site_packages
+
     import calendar
     import collections
     import getopt
@@ -52,11 +52,11 @@ try:
     from functools import reduce
     from pkg.misc import PipeError, emsg, msg
     from urllib.parse import quote
-    from pkg.client.pkgdefs import (EXIT_OK, EXIT_OOPS, EXIT_BADOPT,
-        EXIT_PARTIAL)
+    from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_FATAL
 except KeyboardInterrupt:
     import sys
-    sys.exit(EXIT_OOPS)
+    sys.exit(1)  # EXIT_OOPS
+
 
 class PkgmergeException(Exception):
     """An exception raised if something goes wrong during the merging
@@ -95,7 +95,7 @@ def cleanup():
             # ditch attempt anyway.
             break
 
-def usage(errmsg="", exitcode=2):
+def usage(errmsg="", exitcode=EXIT_BADOPT):
     """Emit a usage message and optionally prefix it with a more specific
     error message.  Causes program to exit."""
 
@@ -982,15 +982,15 @@ if __name__ == "__main__":
         __ret = EXIT_OOPS
     except (PipeError, KeyboardInterrupt):
         __ret = EXIT_OOPS
-    except SystemExit as __e:
-        raise __e
+    except SystemExit:
+        raise
     except EnvironmentError as __e:
         error(str(apx._convert_error(__e)))
         __ret = EXIT_OOPS
-    except Exception as __e:
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message(), exitcode=None)
-        __ret = 99
+        __ret = EXIT_FATAL
     finally:
         cleanup()
 

oxidecomputer/pkg5

src/pkgrepo.py
@@ -24,18 +24,64 @@
 # Copyright (c) 2010, 2020, Oracle and/or its affiliates. All rights reserved.
 # Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
 #
-import pkg.site_paths
 
-pkg.site_paths.init()
+try:
+    import pkg.site_paths
+
+    pkg.site_paths.init()
+    import atexit
+    import collections
+    import copy
+    import errno
+    import getopt
+    import gettext
+    import locale
+    import logging
+    import os
+    import operator
+    import re
+    import shlex
+    import shutil
+    import sys
+    import tempfile
+    import textwrap
+    import traceback
+    import warnings
+    import itertools
+    from importlib import reload
+
+    from pkg.client import global_settings
+    from pkg.client.debugvalues import DebugValues
+    from pkg.misc import msg, PipeError
+    from prettytable import PrettyTable
+    import pkg
+    import pkg.catalog
+    import pkg.client.api_errors as apx
+    import pkg.client.pkgdefs as pkgdefs
+    import pkg.client.progress
+    import pkg.client.publisher as publisher
+    import pkg.client.transport.transport as transport
+    import pkg.fmri as fmri
+    import pkg.json as json
+    import pkg.misc as misc
+    import pkg.server.repository as sr
+
+    from pkg.client.pkgdefs import (
+        EXIT_OK,
+        EXIT_OOPS,
+        EXIT_BADOPT,
+        EXIT_PARTIAL,
+        EXIT_FATAL,
+    )
+except KeyboardInterrupt:
+    import sys
+
+    sys.exit(1)  # EXIT_OOPS
 
 PKG_CLIENT_NAME = "pkgrepo"
 
-# pkgrepo exit codes
-EXIT_OK = 0
-EXIT_OOPS = 1
-EXIT_BADOPT = 2
-EXIT_PARTIAL = 3
-EXIT_DIFF = 10
+# pkgrepo specific exit codes
+EXIT_DIFF = 10  # Differences found for pkgrepo diff.
 
 # listing constants
 LISTING_FORMATS = ("default", "json", "json-formatted", "tsv")
@@ -48,43 +94,6 @@ diff_type_f = {MINUS: "- ", PLUS: "+ ", COMMON: ""}
 
 # globals
 tmpdirs = []
-
-import atexit
-import collections
-import copy
-import errno
-import getopt
-import gettext
-import locale
-import logging
-import os
-import operator
-import re
-import shlex
-import shutil
-import sys
-import tempfile
-import textwrap
-import traceback
-import warnings
-import itertools
-from importlib import reload
-
-from pkg.client import global_settings
-from pkg.client.debugvalues import DebugValues
-from pkg.misc import msg, PipeError
-from prettytable import PrettyTable
-import pkg
-import pkg.catalog
-import pkg.client.api_errors as apx
-import pkg.client.pkgdefs as pkgdefs
-import pkg.client.progress
-import pkg.client.publisher as publisher
-import pkg.client.transport.transport as transport
-import pkg.fmri as fmri
-import pkg.json as json
-import pkg.misc as misc
-import pkg.server.repository as sr
 
 logger = global_settings.logger
 
@@ -130,7 +139,7 @@ def get_tracker(quiet=False):
     return progtrack
 
 
-def usage(usage_error=None, cmd=None, retcode=2, full=False):
+def usage(usage_error=None, cmd=None, retcode=EXIT_BADOPT, full=False):
     """Emit a usage message and optionally prefix it with a more
     specific error message.  Causes program to exit.
     """
@@ -2708,8 +2717,8 @@ def handle_errors(func, *args, **kwargs):
                 raise apx._convert_error(__e)
             error("\n" + misc.out_of_memory())
             __ret = EXIT_OOPS
-    except SystemExit as __e:
-        raise __e
+    except SystemExit:
+        raise
     except (IOError, PipeError, KeyboardInterrupt) as __e:
         # Don't display any messages here to prevent possible further
         # broken pipe (EPIPE) errors.
@@ -2735,10 +2744,10 @@ def handle_errors(func, *args, **kwargs):
     except (apx.ApiException, sr.RepositoryError) as __e:
         error(str(__e))
         __ret = EXIT_OOPS
-    except:
+    except Exception:
         traceback.print_exc()
         error(traceback_str)
-        __ret = 99
+        __ret = EXIT_FATAL
     return __ret
 
 

src/publish.py
@@ -24,34 +24,43 @@
 # Copyright (c) 2007, 2019, Oracle and/or its affiliates. All rights reserved.
 #
 
-import pkg.site_paths
+try:
+    import pkg.site_paths
 
-pkg.site_paths.init()
-import fnmatch
-import getopt
-import gettext
-import locale
-import os
-import sys
-import traceback
-import warnings
-import errno
-from pkg.client.pkgdefs import EXIT_OOPS, EXIT_OK, EXIT_PARTIAL, EXIT_BADOPT
+    pkg.site_paths.init()
+    import fnmatch
+    import getopt
+    import gettext
+    import locale
+    import os
+    import sys
+    import traceback
+    import warnings
+    import errno
+    from importlib import reload
+    import pkg.actions
+    import pkg.bundle
+    import pkg.client.api_errors as apx
+    import pkg.fmri
+    import pkg.manifest
+    import pkg.misc as misc
+    import pkg.publish.transaction as trans
+    import pkg.client.transport.transport as transport
+    import pkg.client.publisher as publisher
+    from pkg.misc import msg, emsg, PipeError
+    from pkg.client import global_settings
+    from pkg.client.debugvalues import DebugValues
+    from pkg.client.pkgdefs import (
+        EXIT_OK,
+        EXIT_OOPS,
+        EXIT_BADOPT,
+        EXIT_PARTIAL,
+        EXIT_FATAL,
+    )
+except KeyboardInterrupt:
+    import sys
 
-from importlib import reload
-
-import pkg.actions
-import pkg.bundle
-import pkg.client.api_errors as apx
-import pkg.fmri
-import pkg.manifest
-import pkg.misc as misc
-import pkg.publish.transaction as trans
-import pkg.client.transport.transport as transport
-import pkg.client.publisher as publisher
-from pkg.misc import msg, emsg, PipeError
-from pkg.client import global_settings
-from pkg.client.debugvalues import DebugValues
+    sys.exit(1)  # EXIT_OOPS
 
 nopub_actions = ["unknown"]
 
@@ -674,7 +683,7 @@ def trans_import(repo_uri, args, visitors=[]):
         print(
             _("No transaction ID specified in $PKG_TRANS_ID"), file=sys.stderr
         )
-        sys.exit(1)
+        sys.exit(EXIT_OOPS)
 
     opts, pargs = getopt.getopt(args, "T:", ["target="])
 
@@ -961,12 +970,12 @@ if __name__ == "__main__":
     except MemoryError:
         error("\n" + misc.out_of_memory())
         __ret = EXIT_OOPS
-    except SystemExit as _e:
-        raise _e
-    except:
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message())
-        __ret = 99
+        __ret = EXIT_FATAL
     sys.exit(__ret)
 
 # Vim hints

src/util/publish/pkgdiff.py
@@ -24,26 +24,36 @@
 # Copyright (c) 2009, 2019, Oracle and/or its affiliates. All rights reserved.
 #
 
-import pkg.site_paths
+try:
+    import pkg.site_paths
 
-pkg.site_paths.init()
-import getopt
-import gettext
-import locale
-import sys
-import traceback
-import warnings
-from functools import cmp_to_key
+    pkg.site_paths.init()
+    import getopt
+    import gettext
+    import locale
+    import sys
+    import traceback
+    import warnings
+    from functools import cmp_to_key
+    import pkg.actions
+    import pkg.variant as variant
+    import pkg.client.api_errors as apx
+    import pkg.manifest as manifest
+    import pkg.misc as misc
+    from pkg.misc import PipeError, CMP_UNSIGNED, CMP_ALL
+    from collections import defaultdict
+    from itertools import product
+    from pkg.client.pkgdefs import (
+        EXIT_OK,
+        EXIT_OOPS,
+        EXIT_BADOPT,
+        EXIT_PARTIAL,
+        EXIT_FATAL,
+    )
+except KeyboardInterrupt:
+    import sys
 
-import pkg.actions
-import pkg.variant as variant
-import pkg.client.api_errors as apx
-import pkg.manifest as manifest
-import pkg.misc as misc
-from pkg.misc import PipeError, CMP_UNSIGNED, CMP_ALL
-from collections import defaultdict
-from itertools import product
-from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_PARTIAL
+    sys.exit(1)  # EXIT_OOPS
 
 
 def usage(errmsg="", exitcode=EXIT_BADOPT):
@@ -422,12 +432,12 @@ if __name__ == "__main__":
         exit_code = main_func()
     except (PipeError, KeyboardInterrupt):
         exit_code = EXIT_OOPS
-    except SystemExit as __e:
-        exit_code = __e
-    except Exception as __e:
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message(), exitcode=None)
-        exit_code = 99
+        exit_code = EXIT_FATAL
 
     sys.exit(exit_code)
 

src/util/publish/pkgfmt.py
@@ -24,9 +24,39 @@
 # Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 #
 
-import pkg.site_paths
+try:
+    import pkg.site_paths
 
-pkg.site_paths.init()
+    pkg.site_paths.init()
+
+    import copy
+    import errno
+    import getopt
+    import gettext
+    import io
+    import locale
+    import operator
+    import os
+    import re
+    import sys
+    import tempfile
+    import traceback
+    import warnings
+    from difflib import unified_diff
+    from functools import cmp_to_key
+
+    import pkg
+    import pkg.actions
+    import pkg.misc as misc
+    import pkg.portable
+    from pkg.misc import emsg, PipeError
+    from pkg.actions.generic import quote_attr_value
+    from pkg.actions.depend import known_types as dep_types
+    from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_FATAL
+except KeyboardInterrupt:
+    import sys
+
+    sys.exit(1)  # EXIT_OOPS
 
 # Prefixes should be ordered alphabetically with most specific first.
 DRIVER_ALIAS_PREFIXES = (
@@ -55,36 +85,6 @@ DRIVER_ALIAS_PREFIXES = (
 # 5) variant & facet tags appear at the end of actions
 # 6) multi-valued tags appear at the end aside from the above
 # 7) key attribute tags come first
-
-try:
-    import copy
-    import errno
-    import getopt
-    import gettext
-    import io
-    import locale
-    import operator
-    import os
-    import re
-    import sys
-    import tempfile
-    import traceback
-    import warnings
-    from difflib import unified_diff
-    from functools import cmp_to_key
-
-    import pkg
-    import pkg.actions
-    import pkg.misc as misc
-    import pkg.portable
-    from pkg.misc import emsg, PipeError
-    from pkg.actions.generic import quote_attr_value
-    from pkg.actions.depend import known_types as dep_types
-    from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_PARTIAL
-except KeyboardInterrupt:
-    import sys
-
-    sys.exit(EXIT_OOPS)
 
 FMT_V1 = "v1"
 FMT_V2 = "v2"
@@ -795,13 +795,13 @@ if __name__ == "__main__":
     except (PipeError, KeyboardInterrupt):
         # We don't want to display any messages here to prevent
         # possible further broken pipe (EPIPE) errors.
-        __ret = 1
-    except SystemExit as _e:
-        raise _e
-    except:
+        __ret = EXIT_OOPS
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message(), exitcode=None)
-        __ret = 99
+        __ret = EXIT_FATAL
 
     sys.exit(__ret)
 

src/util/publish/pkgmerge.py
@@ -23,11 +23,11 @@
 # Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
 #
 
-import pkg.site_paths
-
-pkg.site_paths.init()
-
 try:
+    import pkg.site_paths
+
+    pkg.site_paths.init()
+
     import calendar
     import collections
     import getopt
@@ -53,11 +53,11 @@ try:
     from functools import reduce
     from pkg.misc import PipeError, emsg, msg
     from urllib.parse import quote
-    from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_PARTIAL
+    from pkg.client.pkgdefs import EXIT_OK, EXIT_OOPS, EXIT_BADOPT, EXIT_FATAL
 except KeyboardInterrupt:
     import sys
 
-    sys.exit(EXIT_OOPS)
+    sys.exit(1)  # EXIT_OOPS
 
 
 class PkgmergeException(Exception):
@@ -100,7 +100,7 @@ def cleanup():
             break
 
 
-def usage(errmsg="", exitcode=2):
+def usage(errmsg="", exitcode=EXIT_BADOPT):
     """Emit a usage message and optionally prefix it with a more specific
     error message.  Causes program to exit."""
 
@@ -1037,15 +1037,15 @@ if __name__ == "__main__":
         __ret = EXIT_OOPS
     except (PipeError, KeyboardInterrupt):
         __ret = EXIT_OOPS
-    except SystemExit as __e:
-        raise __e
+    except SystemExit:
+        raise
     except EnvironmentError as __e:
         error(str(apx._convert_error(__e)))
         __ret = EXIT_OOPS
-    except Exception as __e:
+    except Exception:
         traceback.print_exc()
         error(misc.get_traceback_message(), exitcode=None)
-        __ret = 99
+        __ret = EXIT_FATAL
     finally:
         cleanup()
 